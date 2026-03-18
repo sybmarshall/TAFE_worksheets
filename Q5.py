1f1372cedf08b37f8152602f6b6e60b89814a55c
20168004 <20168004@tafe.wa.edu.au>
2026-03-18 20:25:19 +0800

    stopped 'quit' from being added if typed

--- Q5.py
+++ Q5.py
@@ -2,11 +2,11 @@ file = open("my_file.txt", "a")
 running = True
 while running == True:
     user_addition = input("What would you like to add? ")
-    file.write(user_addition)
     if user_addition.lower() == "quit":
         print("OK, bye!")
         running = False
     else:
-        continue  
+        file.write(user_addition)
+        continue
 
 file.close()
